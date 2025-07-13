ba8dc2f55edcbfa4498c584c17d499ae0221457e
cian-oL <cianoleary2010@gmail.com>
2025-07-13 20:19:14 +0100

    Landing & auth redirect WIP

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,8 +1,45 @@
+import landingImage from "@/assets/Miljenko-and-Dobrila.png";
+import appLogo from "@/assets/appLogo.svg";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+
 const LandingPage = () => {
   return (
-    <div className="bg-amber-700">
-      Welcome to Hrvatsk-AI, your interactive AI companion to learn Croatian
-      with
+    <div
+      className="flex h-screen w-full items-center justify-center overflow-hidden"
+      style={{
+        backgroundImage: `url(${landingImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Card className="bg-background w-2/3 -translate-y-32 transform border-b-2 border-blue-400 sm:max-w-100">
+        <CardHeader>
+          <CardTitle>Welcome to Hrvatsk-AI</CardTitle>
+          <CardDescription>
+            Your interactive AI companion to learn Croatian with!
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-between">
+          <Button asChild>
+            <Link href="/login">Get Started</Link>
+          </Button>
+          <Image
+            src={appLogo}
+            alt="App Logo"
+            className="h-16 dark:brightness-100 dark:invert"
+          />
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,21 +5,31 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+
+import appLogo from "@/assets/appLogo.svg";
 
 const Header = () => {
   return (
-    <header className="flex h-16 items-center justify-end gap-4 p-4">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton>
-          <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <header className="bg-background w-full border-b-2 border-blue-400">
+      <div className="container mx-2 flex h-20 items-center justify-between px-2">
+        <Link href="/">
+          <Image src={appLogo} alt="App Logo" height={80} width={80} />
+        </Link>
+
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </header>
   );
 };
