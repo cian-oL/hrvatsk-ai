71b5f37e664083e8aa356141b81b92118eef67f7
mecastor <mecastor87@gmail.com>
2025-07-20 11:01:09 +0100

    Added temporary not found and under construction pages/components

---
+++ src/app/not-found.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import boatImage from "@/assets/404-boat.svg";
+import { Button } from "@/components/ui/button";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex items-center justify-center p-6">
+      <div className="w-full max-w-3xl rounded-xl border-2 border-blue-400 p-8 shadow-xl backdrop-blur-sm">
+        <div className="text-center">
+          <h1 className="mb-2 text-6xl font-bold">404 - Not Found</h1>
+        </div>
+        <div className="my-8 overflow-hidden rounded-lg">
+          <div className="my-8 flex items-center justify-center overflow-hidden rounded-lg">
+            <Image src={boatImage} width={400} alt="Sailboat" />
+          </div>
+        </div>
+        <div className="text-center">
+          <Button asChild>
+            <Link href="/chat">Return Home</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
